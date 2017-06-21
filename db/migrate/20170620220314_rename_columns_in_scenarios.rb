fa39b1a33bbbbb075c1f38d4ff3399bd117abb64
xzakm3 <zakyz.martin@gmail.com>
2017-06-21 18:39:29 +0200

    Zmazani migraci, pokus o push na heroku 3

--- db/migrate/20170620220314_rename_columns_in_scenarios.rb
+++
@@ -1,7 +0,0 @@
-class RenameColumnsInScenarios < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :scenarios, :number_of_up, :user_id
-    rename_column :scenarios, :scenario_id, :power
-    remove_column(:scenarios, :entry_room_id)
-  end
-end
--- db/migrate/20170620220456_add_foreign_key_to_scenarios.rb
+++
@@ -1,7 +0,0 @@
-class AddForeignKeyToScenarios < ActiveRecord::Migration[5.0]
-  def change
-    remove_column(:scenarios, :user_id)
-    add_reference :scenarios, :user, index: true
-    add_foreign_key :scenarios, :users
-  end
-end
