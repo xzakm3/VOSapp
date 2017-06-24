b2dfc90d38c9cb56ecd8b9b8123d84b109b071f9
xzakm3 <zakyz.martin@gmail.com>
2017-06-24 21:53:13 +0200

    rozdelenie js suboru a css suboru do prislusnych suborov

--- app/assets/javascripts/appliances.coffee
+++ app/assets/javascripts/appliances.coffee
@@ -1,3 +1,45 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+$(document).on "click", ".update-appliance-button", (e)->
+  element = e.target.closest('.col-md-3')
+  countObject = $(element).prev()
+  performanceObject = $(countObject).prev();
+  applianceObject = $(performanceObject).prev();
+  roomObject = $('.nav-pills > li.active');
+  $('#text').html(name);
+  $('#editModal #room').val($.trim(roomObject.text()));
+  $('#editModal #name').val($.trim(applianceObject.text()));
+  $('#editModal #performance').val(parseInt(performanceObject.text()));
+  $('#editModal #count').val(parseInt(countObject.text()));
+  appliance_id = $(this).data('appliance-id');
+  $('form').attr 'action', (i, value)->
+    value = value.substr(0, value.lastIndexOf("/"));
+    return value + "/" + appliance_id;
+  $('#hidden-entry-id').val($(this).data('entry-id'));
+  $('#hidden-room-id').val($(this).data('room-id'));
+  $('#editModal #hidden-room-name').val($.trim(roomObject.text()));
+
+
+$(document).on "click", ".rooms-nav-link", ->
+  $('#rooms-span').attr('class', $(this).text());
+
+
+$(document).on "click", "#add-button-row", ->
+  $('#smallModalLabel').html($('#rooms-span').attr('class'));
+  $('#smallModal #hidden-room-name').val($('#smallModalLabel').text());
+
+$(document).on "click", ".nav-pills > li > a", ->
+  $('.nav-pills > li.active').removeClass("active");
+  $(this).parent().addClass("active");
+
+
+$(document).on "click", "#appliance", (e)->
+  $('#editModal form').attr 'action', (i, value)->
+    return value + "/1";
+
+
+$(document).on "change", "#bigModal input#room", ->
+  $('#bigModal #bigModalLabel').text $(this).val();
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,50 +18,6 @@
 //= require addButtons
 //= require_tree .
 
-$(document).on("click", ".rooms-nav-link", function(){
-    $('#rooms-span').attr('class', $(this).text());
-});
-
-$(document).on("click", "#add-button-row", function(){
-    $('#smallModalLabel').html($('#rooms-span').attr('class'));
-    $('#smallModal #hidden-room-name').val($('#smallModalLabel').text());
-});
-
-$(document).on("click", ".nav-pills > li > a", function(){
-    $('.nav-pills > li.active').removeClass("active");
-    $(this).parent().addClass("active");
-});
-
-$(document).on("click", "#appliance", function(e){
-    $('#editModal form').attr('action', function(i, value) {
-        return value + "/1";
-    });
-})
-
-$(document).on("click", ".update-appliance-button", function(e){
-    var element = e.target.closest('.col-md-3');
-    var countObject = $(element).prev();
-    var performanceObject = $(countObject).prev();
-    var applianceObject = $(performanceObject).prev();
-    var roomObject = $('.nav-pills > li.active');
-    //$('#text').html(name);
-    $('#editModal #room').val($.trim(roomObject.text()));
-    $('#editModal #name').val($.trim(applianceObject.text()));
-    $('#editModal #performance').val(parseInt(performanceObject.text()));
-    $('#editModal #count').val(parseInt(countObject.text()));
-    var appliance_id = $(this).data('appliance-id');
-    $('form').attr('action', function(i, value) {
-        value = value.substr(0, value.lastIndexOf("/"));
-        return value + "/" + appliance_id;
-    });
-    $('#hidden-entry-id').val($(this).data('entry-id'));
-    $('#hidden-room-id').val($(this).data('room-id'));
-    $('#editModal #hidden-room-name').val($.trim(roomObject.text()));
-});
-
-$(document).on("change", "#bigModal input#room", function(){
-        $('#bigModal #bigModalLabel').text($(this).val());
-});
 
 
 
